f5769dc9d610019124ad8397056f61c5f90016e4
sheetalr <sheetal@blackorchidz.com>
2014-11-02 17:14:09 +0530

    using ionic and angular framework

---
+++ www/js/services.js
@@ -0,0 +1,26 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Sheetal' },
+    { id: 1, name: 'Dines' },
+    { id: 2, name: 'Kavi' },
+    { id: 3, name: 'Prasanna' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+});
